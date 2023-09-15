ba35875368295783d1d2710b6667b717fcd311ea
Ryan Brandle <ryanbrandle@Ryans-MacBook-Pro.local>
2023-09-15 17:58:13 -0600

    started stubbing layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,29 @@
+const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
+
 const App = () => {
-	return <div>Hello React!</div>;
+	const step = 3;
+
+	return (
+		<div className='steps'>
+			<div className='numbers'>
+				<div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+				<div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+				<div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+			</div>
+
+			<p className='message'>
+				Step {step}: {messages[step - 1]}
+			</p>
+
+			<div className='buttons'>
+				<button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={''}>
+					Previous
+				</button>
+				<button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={''}>
+					Next
+				</button>
+			</div>
+		</div>
+	);
 };
 export default App;
