c836c26b8e4e9099f2ba2c258fbccc4265b0efac
Ryan Brandle <r.brandle@gmail.com>
2023-09-18 14:58:58 -0600

    added onClick functionality to increaes and decrease steps, added disabled states for buttons

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { useState } from 'react';
+
 const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new income 🤑'];
 
 const App = () => {
-	const step = 3;
+	const [step, setStep] = useState(1);
+
+	const handlePrev = () => {
+		if (step > 1) {
+			setStep(step - 1);
+		}
+	};
+
+	const handleNext = () => {
+		if (step < 3) {
+			setStep(step + 1);
+		}
+	};
 
 	return (
 		<div className='steps'>
 			<div className='numbers'>
-				<div className={`${step >= 1 ? 'active' : ''}`}>1</div>
-				<div className={`${step >= 2 ? 'active' : ''}`}>2</div>
-				<div className={`${step >= 3 ? 'active' : ''}`}>3</div>
+				<div className={step >= 1 ? 'active' : ''}>1</div>
+				<div className={step >= 2 ? 'active' : ''}>2</div>
+				<div className={step >= 3 ? 'active' : ''}>3</div>
 			</div>
 
 			<p className='message'>
@@ -16,10 +30,10 @@ const App = () => {
 			</p>
 
 			<div className='buttons'>
-				<button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={''}>
+				<button onClick={handlePrev} disabled={step === 1}>
 					Previous
 				</button>
-				<button style={{ backgroundColor: '#7950f2', color: 'white' }} onClick={''}>
+				<button onClick={handleNext} disabled={step === 3}>
 					Next
 				</button>
 			</div>
