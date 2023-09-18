33fba9fc329de4aa42c5411adac8d11ff4c947bd
Ryan Brandle <ryanbrandle@Ryans-MacBook-Pro.local>
2023-09-18 17:07:06 -0600

    updated usage of state variables as a function that passes a current version of state as a safety net

--- src/App.js
+++ src/App.js
@@ -4,40 +4,48 @@ const messages = ['Learn React ⚛️', 'Apply for jobs 💼', 'Invest your new 
 
 const App = () => {
 	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 
 	const handlePrev = () => {
 		if (step > 1) {
-			setStep(step - 1);
+			setStep((s) => s - 1);
 		}
 	};
 
 	const handleNext = () => {
 		if (step < 3) {
-			setStep(step + 1);
+			setStep((s) => s + 1);
 		}
 	};
 
 	return (
-		<div className='steps'>
-			<div className='numbers'>
-				<div className={step >= 1 ? 'active' : ''}>1</div>
-				<div className={step >= 2 ? 'active' : ''}>2</div>
-				<div className={step >= 3 ? 'active' : ''}>3</div>
-			</div>
+		<>
+			<button className='close' onClick={() => setIsOpen((is) => !is)}>
+				&times;
+			</button>
+			{isOpen && (
+				<div className='steps'>
+					<div className='numbers'>
+						<div className={step >= 1 ? 'active' : ''}>1</div>
+						<div className={step >= 2 ? 'active' : ''}>2</div>
+						<div className={step >= 3 ? 'active' : ''}>3</div>
+					</div>
 
-			<p className='message'>
-				Step {step}: {messages[step - 1]}
-			</p>
+					<p className='message'>
+						Step {step}: {messages[step - 1]}
+					</p>
 
-			<div className='buttons'>
-				<button onClick={handlePrev} disabled={step === 1}>
-					Previous
-				</button>
-				<button onClick={handleNext} disabled={step === 3}>
-					Next
-				</button>
-			</div>
-		</div>
+					<div className='buttons'>
+						<button onClick={handlePrev} disabled={step === 1}>
+							Previous
+						</button>
+						<button onClick={handleNext} disabled={step === 3}>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 export default App;
